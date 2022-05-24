ab73bea182cfca04187f97fdb237a282e8295e19
ajain96 <anubhavjain2809@gmail.com>
2022-05-24 15:05:15 +0530

    commit message

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        static void Main1(string[] args)
+        {
+            int[] myArray = new int[] { 0, 1, 2, 3, 13, 8, 5 };
+            int largest = int.MinValue;
+            int second = int.MinValue;
+            foreach (int i in myArray)
+            {
+                if (i > largest)
+                {
+                    second = largest;
+                    largest = i;
+                }
+                else if (i > second)
+                    second = i;
+            }
+
+            System.Console.WriteLine(second);
+        }
+    }
+}
